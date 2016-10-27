7ba743cfdf68b7f374a9fde500f4554b561ba763
schum0689 <schum0689@HRH-IC0027344.wrdsb.ca>
2016-10-27 09:39:56 -0400

    10/27. Quiz 3 complete.

--- src/Q1.java
+++ src/Q1.java
@@ -26,24 +26,30 @@ public class Q1 {
 
         // Plant 4 rows of seeds
         for (int plantRow = 0; plantRow < 4; plantRow = plantRow + 1) {
+            
             // Fill one row by planting 5 seeds
             for (int plantSeed = 0; plantSeed < 5; plantSeed = plantSeed + 1) {
+                
                 // Plant one seed
                 rick.putThing();
-                // If rick is not at the end of the row (avenue 5)
+                
+                // If rick is not at the end of the row (avenue 5),
                 if (rick.getAvenue() != 5) {
                     // Move forward
                     rick.move();
                     // If rick is at the end of the row (avenue 5)
                 } else {
-                    // MOVE TO NEW ROW
+                    
+                    // Move down to the next row
                     rick.turnRight();
                     rick.move();
                     rick.turnRight();
+                    
                     // Move to start of row (avenue 1) 
                     do {
                         rick.move();
                     } while (rick.getAvenue() != 1);
+                    
                     // Turn around to plant new row of seeds
                     rick.turnAround();
                 }
