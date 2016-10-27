9e43772e6a2087ea07758efd0dc66c23e27db6ed
awadb3223 <awadb3223@HRH-IC0027321.wrdsb.ca>
2016-10-27 09:49:08 -0400

    Quiz3_Beshoy Awad

--- src/Quiz.java
+++ src/Quiz.java
@@ -23,34 +23,34 @@ public class Quiz {
         //Create a special robot WITH 20 things in his backpack
         RobotSE bob = new RobotSE(field, 1, 1, Direction.EAST, 20);
 
-      
-            //Create a big loop to make robot plant all four rows
-            for (int a = 0; a <= 3; a = a + 1) {
-                //Create for loop to plant 5 in a horizontal row
-                for (int i = 0; i <= 4; i = i + 1) {
-                    bob.putThing();
-                    bob.move();
-                }
-                //get robot to move to second row
-                bob.turnRight();
-                bob.move();
-                bob.turnRight();
-                bob.move();
-                //loop again
-                for (int m = 0; m <= 4; m = m + 1) {
-                    bob.putThing();
-                    bob.move();
-                }
-                //get robot to move to third row
-                bob.turnLeft();
-                bob.move();
-                bob.turnLeft();
-                bob.move();
 
-                //Create if statement to stop robot from dying
-                if (bob.countThingsInBackpack() == 0){
-                   break; 
-                }
+        //Create a big loop to make robot plant all four rows
+        for (int a = 0; a <= 3; a = a + 1) {
+            //Create for loop to plant 5 in a horizontal row
+            for (int i = 0; i <= 4; i = i + 1) {
+                bob.putThing();
+                bob.move();
+            }
+            //get robot to move to second row
+            bob.turnRight();
+            bob.move();
+            bob.turnRight();
+            bob.move();
+            //loop again
+            for (int m = 0; m <= 4; m = m + 1) {
+                bob.putThing();
+                bob.move();
+            }
+            //get robot to move to third row
+            bob.turnLeft();
+            bob.move();
+            bob.turnLeft();
+            bob.move();
+
+            //Create if statement to stop robot from dying
+            if (bob.countThingsInBackpack() == 0) {
+                break;
             }
         }
     }
+}
